2df43a7fe7ccd8236689b62c81e084dca3847240
tshefi <tshefi@redhat.com>
2019-02-03 10:14:03 +0200

    Initial commit

---
+++ pre_bm_osp14.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Automated script to pre_deploy OSP14 on BM
+# Tzach Shefi
+# 2/2018
+# ver: 1.0
+# This script does all the pre UC deploy tasks and reboots.
+# a second bash script will continute work post reboot.
+
+
+# RFE - Clean hosts via foreman ?
+
+
