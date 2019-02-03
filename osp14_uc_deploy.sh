7d99efd1cc8de49f7a6fd3e22f4414942c0bf3c7
tshefi <tshefi@redhat.com>
2019-02-03 10:56:33 +0200

    Initial commit

--- osp14_uc_deploy.sh
+++ osp14_uc_deploy.sh
@@ -7,35 +7,9 @@
 
 # RFE validate the pre_bm_osp14.sh has been run
 
-# All step numbers reference install guide:
-#https://access.redhat.com/documentation/en-us/red_hat_openstack_platform/14-beta/html/director_installation_and_usage/preparing-for-director-installation#preparing-the-undercloud
 
 
 
-
-# 3.1. Preparing the undercloud
-#create user stack pass stack
-useradd stack
-passwd stack
-echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
-chmod 0440 /etc/sudoers.d/stack
-echo  "127.0.0.1   "$(hostname)"  "$(hsotname -s)  >> /etc/hosts
-
-#Create stack user
-su - stack
-mkdir ~/images
-mkdir ~/templates
-
-
-#Bring repos
-
-
-#Get post reboot next script
-
-
-#update+reboot
-sudo yum update -y
-sudo reboot
 
 
 
--- pre_bm_osp14.sh
+++ pre_bm_osp14.sh
@@ -9,4 +9,38 @@
 
 # RFE - Clean hosts via foreman ?
 
+#Clean puppets from foreman!!
+yum remove puppet* -y
 
+# All step numbers reference install guide:
+#https://access.redhat.com/documentation/en-us/red_hat_openstack_platform/14-beta/html/director_installation_and_usage/preparing-for-director-installation#preparing-the-undercloud
+
+# 3.1. Preparing the undercloud
+#create user stack pass stack
+useradd stack
+passwd stack
+echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
+chmod 0440 /etc/sudoers.d/stack
+echo  "127.0.0.1   "$(hostname)"  "$(hsotname -s)  >> /etc/hosts
+
+#Create stack user
+su - stack
+mkdir ~/images
+mkdir ~/templates
+
+
+#Fix remove/reinstall repos
+sudo yum remove -y rhos-release
+sudo rpm -ivh http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm
+sudo rm -rf /etc/yum.repos.d/*
+sudo rm -rf /var/cache/yum/*
+sudo rhos-release 10-director > /home/stack/version.txt
+
+
+#Get post reboot next script
+wget
+chmod +x
+
+#update+reboot
+sudo yum update -y
+sudo reboot
